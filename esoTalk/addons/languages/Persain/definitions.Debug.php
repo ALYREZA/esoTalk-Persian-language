2845074c367d64cf8753874b319aec8c4a240f49
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:44:03 +0330

    Create definitions.Debug.php

---
+++ esoTalk/addons/languages/Persain/definitions.Debug.php
@@ -0,0 +1,11 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the Debug plugin.
+
+$definitions["MySQL queries"] = "MySQL queries";
+$definitions["Page loaded in %s seconds"] = "صفحه در  %s ثانیه بارگذاری شد.";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES information";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE information";
