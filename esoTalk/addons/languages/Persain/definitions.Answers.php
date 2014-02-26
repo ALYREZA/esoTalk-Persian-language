8ad228ae3d30ebadb5e8f6c75c6a55e90c3cda96
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:42:32 +0330

    Create definitions.Answers.php

---
+++ esoTalk/addons/languages/Persain/definitions.Answers.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the Answers plugin.
+
+$definitions["Answer"] = "پاسخ";
+$definitions["Answered"] = "پاسخ داده شده";
+$definitions["This post answered my question"] = "این جواب سوال من بود";
+$definitions["Answered by %s"] = "پاسخ داده شده بوسیله ی %s";
+$definitions["See post in context"] = "دیدن جزئیات مطلب";
+$definitions["Remove answer"] = "حذف پاسخ";
