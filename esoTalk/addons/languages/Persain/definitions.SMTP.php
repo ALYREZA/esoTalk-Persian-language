b3c51fd922a9621aec32aa82d09274fe3ade36c4
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:45:57 +0330

    Create definitions.SMTP.php

---
+++ esoTalk/addons/languages/Persain/definitions.SMTP.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the SMTP plugin.
+
+$definitions["Server"] = "Server";
+$definitions["Port"] = "Port";
+$definitions["Authentication"] = "Authentication";
+$definitions["Normal"] = "Normal";
+$definitions["TLS"] = "TLS";
+$definitions["SSL"] = "SSL";
