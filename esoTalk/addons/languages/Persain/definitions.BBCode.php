4e9b829d184b8ad02914a56dd87af9047e143297
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:43:34 +0330

    Create definitions.BBCode.php

---
+++ esoTalk/addons/languages/Persain/definitions.BBCode.php
@@ -0,0 +1,14 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the BBCode plugin.
+
+$definitions["Code"] = "Code";
+$definitions["Image"] = "Image";
+$definitions["Link"] = "Link";
+$definitions["Strike"] = "Strike";
+$definitions["Header"] = "Header";
+$definitions["Italic"] = "Italic";
+$definitions["Bold"] = "Bold";
