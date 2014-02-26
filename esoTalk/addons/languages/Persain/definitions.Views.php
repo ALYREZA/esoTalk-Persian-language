b09730946522fddd989410744a65ce8594aab94c
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:46:31 +0330

    Create definitions.Views.php

---
+++ esoTalk/addons/languages/Persain/definitions.Views.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persain Definitions for the Profiles plugin.
+
+$definitions["gambit.order by views"] = "مرتط سازی بر اساس تعداد مشاهده ها";
+$definitions["%s view"] = "%s بار مشاهده شده";
+$definitions["%s views"] = "%s بار مشاهده شده";
