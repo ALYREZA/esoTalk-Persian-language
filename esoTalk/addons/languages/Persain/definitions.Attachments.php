7bb857d7adb9cc833b474cd388376300cfd3052b
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:43:04 +0330

    Create definitions.Attachments.php

---
+++ esoTalk/addons/languages/Persain/definitions.Attachments.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the Answers plugin.
+
+$definitions["Attach a file"] = "پیوست یک فایل";
+$definitions["Drop files to upload"] = "پرتاب یک فایل برای بارگذاری";
+$definitions["message.attachmentNotFound"] = "به خاطر چنیدین دلیل شما نمی توانید این فایل پیوست را مشاهده کنید ، یا از دسترس خارج شده است و یا شما مجاز به دیدن ان فایل نیستید.";
