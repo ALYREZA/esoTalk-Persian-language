fb7339a8978ab2a539494e1026f801fee79a7d77
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:44:27 +0330

    Create definitions.Likes.php

---
+++ esoTalk/addons/languages/Persain/definitions.Likes.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persian Definitions for the Profiles plugin.
+
+$definitions["Unlike"] = "نپسندیدم";
+$definitions["Like"] = "می پسندم";
+$definitions["Members Who Liked This Post"] = "افرادی که این مطلب را پسندیدند.";
+$definitions["%s likes this."] = "%s این مطلب را پسندیدند.";
+$definitions["%s like this."] = "%s ای مطلب را پسندید.";
