915ea8a512b8413fa86c722031bd235a794f02df
Alyreza mahmoudy <fanaticboy666@gmail.com>
2014-02-26 19:45:34 +0330

    Create definitions.Profiles.php

---
+++ esoTalk/addons/languages/Persain/definitions.Profiles.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2014 Alyreza Mahmoudy
+// This file is part of esoTalk. 
+//report bug here:https://github.com/ALYREZA/esoTalk-Persian-language/issues
+
+// Persain Definitions for the Profiles plugin.
+
+$definitions["About"] = "درباره ی من";
+$definitions["Location"] = "زادگاه";
+$definitions["Write something about yourself."] = "چند خطی درباره ی خودت بنویس.";
